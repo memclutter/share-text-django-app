6a07f1ddaf7b1ec4f825e7ce3eabf9a5b36bb198
Memory Clutter <memclutter@gmail.com>
2018-01-31 21:32:53 +0300

    Create entry form

--- assets/create.js
+++ assets/create.js
@@ -1,4 +1,60 @@
+class CreateEntry extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.state = {
+            text: '',
+            isProcessing: false,
+        };
+    }
+
+    handleChange(event) {
+        this.setState({text: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        $.post('/api/entries/', {text: this.state.text})
+            .then(() => {
+                alert('Entry created!');
+                window.location.href = '/';
+            }).catch((err) => {
+
+            });
+    }
+
+    render() {
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <textarea
+                        name="text"
+                        id="text"
+                        className="form-control"
+                        rows="6"
+                        value={this.state.text}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <hr />
+                <div className="form-group text-center">
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                        disabled={this.state.text.length === 0}
+                    >Save</button>
+                </div>
+            </form>
+        );
+    }
+}
+
 ReactDOM.render(
-    <h1>Create!</h1>,
+    <CreateEntry />,
     document.getElementById('root')
 );
